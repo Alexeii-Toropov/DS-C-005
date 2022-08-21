77a0900f0a5b230314008f6dc89d1342fc97183c
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-22 00:02:27 +0300

    Add files via upload

---
+++ C#005.3/Program.cs
@@ -0,0 +1,23 @@
+﻿Random rand = new Random();
+int rand1 = rand.Next(0,99);
+int[] mas = new int[rand1];
+Random ran = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = ran.Next(-1000,1000);
+}
+int max = mas[0];
+int min = mas[0];
+for (int j = 1; j < mas.Length; j++)
+  {
+    if (max < mas[j])
+    {
+      max = mas[j];
+    }
+    if (min > mas[j])
+    {
+    min = mas[j];
+    }
+  }
+int res = max - min;
+Console.Write("Разница между мах и мин элементами массива = " + res);
