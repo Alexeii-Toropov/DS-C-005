d50d6f854cd7709420fd61a48b7fda585a306f00
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-21 16:27:46 +0300

    Add files via upload

---
+++ seminar5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[123];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0,1000);
+}
+int count = 0;
+int j = 0;
+while (j < array.Length)
+{
+    if (array[j] > 10 && array[j] < 99)
+    {
+        count++;
+        j++;
+    }
+    else
+    {
+     j++;
+    }
+}
+Console.Write("Количествго элементов массива на отрезке от 10 до 99 составляет: " + count);
