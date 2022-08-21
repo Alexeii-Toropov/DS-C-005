344465757caf5982bc087caf214f94796b5c839d
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-22 00:01:18 +0300

    Add files via upload

---
+++ C#005.1/Program.cs
@@ -0,0 +1,23 @@
+﻿Random ran = new Random();
+int ran1 = ran.Next(0,99);
+int[] array = new int[ran1];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100,999);
+}
+int count = 0;
+int j = 0;
+while (j < array.Length)
+{
+    if (array[j] % 2 == 0)
+    {
+        count++;
+        j++;
+    }
+    else
+    {
+     j++;
+    }
+}
+Console.Write("Количествго четных элементов массива составляет: " + count);
