8a1020d8851c2bee22733c5bed83c7161e1a8bd3
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-22 00:01:54 +0300

    Add files via upload

---
+++ C#005.2/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rand = new Random();
+int rand1 = rand.Next(0,99);
+int[] mas = new int[rand1];
+Random ran = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = ran.Next(-300,300);
+}
+int j = mas.Length;
+int index = 0;
+int sum = 0;
+while (j > 0)
+{
+    if (mas[index] % 2 == 0)
+    {
+        index++;
+        j--;
+    }
+    else
+    {
+        sum += mas[index];
+        index++;
+        j--;
+    }
+}
+Console.Write("Сумма элементов, стоящих на нечетных позициях = " + sum);
